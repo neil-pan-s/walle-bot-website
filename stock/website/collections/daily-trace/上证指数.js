baedbddbc258e378a2c02b003210d254f1b06644
neil-pan-s <p.changing@qq.com>
2019-10-11 18:58:38 +0800

    :)

---
+++ stock/website/collections/daily-trace/上证指数.js
@@ -0,0 +1,6 @@
+__Walle_Charts_Callback({
+    "name": "上证指数",
+    "total": { "days": 1 , "profit-days": 1, "big-deficit-days": 0, "total-profit": "%0" },
+    "daily-ptcchange": [20191009,0.00,],
+    "daily-profit": [20191009,0,],
+  })
