2eeed51090230dca0586fe55a941f5b26ffc543b
neil-pan-s <p.changing@qq.com>
2019-10-25 20:11:27 +0800

    :)

---
+++ stock/collections/daily-trace/主力净流入榜.js
@@ -0,0 +1,54 @@
+__Walle_Charts_Callback(
+{
+	"name": "主力净流入榜",
+	"profit": {
+		"max": 26.130000000000003,
+		"normal": -27.43,
+		"strategy": 24.310000000000002,
+		"open-crash": 0,
+		"stop-crash": -4.800000000000001,
+		"reverse-crash": 0
+	},
+	"day": {
+		"total": 10,
+		"trade": 3,
+		"rest": 7,
+		"up": 2,
+		"down": 8,
+		"open-crash": 0,
+		"stop-crash": 1,
+		"reverse-crash": 0
+	},
+	"daily-open-profit": [
+		20191014,
+		0,
+		20191015,
+		0.86,
+		20191025,
+		-4.58
+	],
+	"daily-close-profit": [
+		20191014,
+		2.78,
+		20191015,
+		0.9599999999999997,
+		20191025,
+		24.310000000000002
+	],
+	"daily-open-ptcchange": [
+		20191014,
+		0,
+		20191015,
+		0.86,
+		20191025,
+		-5.44
+	],
+	"daily-close-ptcchange": [
+		20191014,
+		2.78,
+		20191015,
+		-1.82,
+		20191025,
+		23.35
+	]
+})
---
+++ stock/collections/daily-trace/跌幅榜.js
@@ -0,0 +1,38 @@
+__Walle_Charts_Callback(
+{
+	"name": "跌幅榜",
+	"profit": {
+		"max": 0,
+		"normal": -252.93,
+		"strategy": -10,
+		"open-crash": -181.39999999999998,
+		"stop-crash": -45.849999999999994,
+		"reverse-crash": 0
+	},
+	"day": {
+		"total": 11,
+		"trade": 1,
+		"rest": 10,
+		"up": 0,
+		"down": 11,
+		"open-crash": 6,
+		"stop-crash": 3,
+		"reverse-crash": 0
+	},
+	"daily-open-profit": [
+		20191011,
+		0
+	],
+	"daily-close-profit": [
+		20191011,
+		-10
+	],
+	"daily-open-ptcchange": [
+		20191011,
+		0
+	],
+	"daily-close-ptcchange": [
+		20191011,
+		-10
+	]
+})
