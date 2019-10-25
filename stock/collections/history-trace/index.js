880430db2a68fb22a67a98ca96aab54a2f48f39c
neil-pan-s <p.changing@qq.com>
2019-10-25 19:25:35 +0800

    :)

--- stock/collections/history-trace/index.js
+++ stock/collections/history-trace/index.js
@@ -1,3 +1,3 @@
 __Walle_Stocks_Callback(
-{ "names": "t1-002197-SZ,t1-600010-SH,t1-600519-SH,t2-big-ptcchange,t2-small-ptcchange" }
+{ "names": "t1-002197-SZ,t1-600010-SH,t1-600519-SH,t3-big-ptcchange,t3-small-ptcchange" }
 )
